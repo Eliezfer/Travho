1589588a79799c894a628a7f27c9883ef6938197
GarciaAlejandro <alejandrogc672@gmail.com>
2019-11-23 12:49:12 -0600

    User Schema

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,9 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
+            $table->string('user');
+            $table->date('birthdate');
+            $table->decimal('cellphone',10,0);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
