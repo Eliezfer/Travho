0bb7ef723ba5307d929a178913bdba8befed70b1
Santiagomrn <santiagomrn98@gmail.com>
2019-12-11 10:55:56 -0600

    w

--- tests/Feature/HouseTest.php
+++ tests/Feature/HouseTest.php
@@ -141,6 +141,136 @@ class HouseTest extends TestCase
 
 
     }
+         /**
+     * CREATE-2
+     */
+    public function test_user_can_create_a_house_is_not_authenticate(){
+        //dado
+        $user = factory(User::class)->create();
+     $houseData =[
+            "data"=>[
+              "description"=> "new house",
+              "price_for_day"=> "22",
+              "status"=> "true",
+              "country"=> "mexico",
+              "state"=> "Yucatán",
+              "municipality"=> "Merida"
+            ]
+              ,
+            "address"=> [
+              "street"=> "29",
+              "cross_street1"=> "31",
+              "cross_street2"=> "33",
+              "house_number"=> "23",
+              "suburb"=> "centro",
+              "postcode"=> "97000"
+            ]
+            ];
+             //cuando
+        $response = $this->actingAs($user)->json('POST', '/api/v1/houses?api_token='.$user['api_token'], $houseData);
+            //entonces
+        $body = $response->decodeResponseJson();
+        $response->assertStatus(201);
+        //verificar la estructura devuelta
+        $response->assertJsonStructure([
+            'id',
+            'id_user',
+            'data'=>[
+                'description',
+                'price_for_day',
+                'status',
+                'country',
+                'state',
+                'municipality',
+            ],
+            'address'=>[
+                'street',
+                'cross_street1',
+                'cross_street2',
+                'house_number',
+                'suburb',
+                'postcode'
+            ],
+            'link'=>[
+                'self'
+            ],
+            'User'=>[
+                'data'=>[
+                    'name',
+                    'user',
+                    'birthdate',
+                    'cellphone',
+                    'email',
+                ],
+                'link'=>[
+                    'self'
+                ]
+            ]
+        ]);
+        $response->assertJson([
+
+                'id'=>$body['id'],
+                'id_user'=>$body['id_user'],
+                'data'=>[
+                    'description'=>$body['data']['description'],
+                    'price_for_day'=>$body['data']['price_for_day'],
+                    'status'=>$body['data']['status'],
+                    'country'=>$body['data']['country'],
+                    'state'=>$body['data']['state'],
+                    'municipality'=>$body['data']['municipality'],
+                ],
+                'address'=>[
+                    'street'=>$body['address']['street'],
+                    'cross_street1'=>$body['address']['cross_street1'],
+                    'cross_street2'=>$body['address']['cross_street2'],
+                    'house_number'=>$body['address']['house_number'],
+                    'suburb'=>$body['address']['suburb'],
+                    'postcode'=>$body['address']['postcode']
+                ],
+                'link'=>[
+                    'self'=>$body['link']['self']
+                ],
+                'User'=>[
+                    'data'=>[
+                        'name'=>$body['User']['data']['name'],
+                        'user'=>$body['User']['data']['user'],
+                        'birthdate'=>$body['User']['data']['birthdate'],
+                        'cellphone'=>$body['User']['data']['cellphone'],
+                        'email'=>$body['User']['data']['email'],
+                    ],
+                    'link'=>[
+                        'self'=>$body['User']['link']['self'],
+                    ]
+                ]
+            ]);
+            $this->assertDatabaseHas(
+             'houses'
+            ,[
+                'id'=>$body['id'],
+                'user_id'=>$body['id_user'],
+                'address_id'=>$body['id'],
+                'description'=>$body['data']['description'],
+                'price_for_day'=>$body['data']['price_for_day'],
+                'status'=>$body['data']['status'],
+                'country'=>$body['data']['country'],
+                'state'=>$body['data']['state'],
+                'municipality'=>$body['data']['municipality'],
+            ]);
+            $this->assertDatabaseHas(
+                'addresses'
+                ,[
+                    'id'=>$body['id'],
+                    'street'=>$body['address']['street'],
+                    'cross_street1'=>$body['address']['cross_street1'],
+                    'cross_street2'=>$body['address']['cross_street2'],
+                    'house_number'=>$body['address']['house_number'],
+                    'suburb'=>$body['address']['suburb'],
+                    'postcode'=>$body['address']['postcode']
+            ]);
+
+
+    }
+
      /**
      * UPDATE-1
      */
